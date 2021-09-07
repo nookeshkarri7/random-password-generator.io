ebff79f4c442bf090921ab18123f96ea94d03135
nookesh <nookeshkarri7@gmail.com>
2021-09-07 22:16:17 +0530

    Finally completed my first version of Random Password Generator

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 let randomPassTxtEl=document.getElementById("randomPassword");
 let selectlengthEl=document.getElementById("selectlength")
+let btnEl=document.getElementById("generaterandomPass")
+
 let lengthel={
     l:0
 }
-selectlengthEl.addEventListener("change",function(event){
-    lengthel.l=selectlengthEl.value
-})
 
-if (lengthel.l===0){
-    randomPassTxtEl.textContent="Please select required Length below";
-}
-else{
-    function generaterandomPass(){
-        let alphabets="ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&()1234567890"
-        let lengthofpass=selectlengthEl.value
+selectlengthEl.addEventListener("change",function(){
+    lengthel.l=parseInt(selectlengthEl.value)
+})
+randomPassTxtEl.textContent="Please Enter Length of Password"
+
+let alphabets="ABCDEFGH45678IJKLM!@#$%NOPQRSTUVWXYZ!@#$%^&()1234567890"
+btnEl.onclick=function(){
+        let lengthofpass=parseInt(selectlengthEl.value)
+        //console.log(lengthofpass)
         let n=alphabets.slice(0,lengthofpass)
+    if (selectlengthEl.value===""){
+        randomPassTxtEl.textContent="Enter Required Length"
+    }
+    else if(lengthofpass<=45){
         let randomPass=""
         for (let each of n){
-            let random=Math.ceil(Math.random()*(alphabets.length))
-            randomPass+=alphabets[random]   
+            let random=Math.ceil(Math.random()*n.length)
+            randomPass=randomPass+alphabets[random]   
         }
-        randomPassTxtEl.textContent=randomPass 
+        randomPassTxtEl.textContent=randomPass
+        selectlengthEl.value="" 
     }
+    else{
+        randomPassTxtEl.textContent="Enter required Length (between 0 to 45 only)"
+    }
+    
 }
